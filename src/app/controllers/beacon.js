f229adb789607cab6fa12387ded66e49bf886740
yuyu <yuyu1911@gmail.com>
2012-08-16 19:45:56 +0800

    加入轨迹收集和播放功能

--- src/app/controllers/beacon.js
+++ src/app/controllers/beacon.js
@@ -4,17 +4,46 @@
  * @createTime : 2012-08-15
  */
 (function(){
-	var beacon = require("../models/beacon.js");
+	var beacon = require("../models/beacon.js"),
+	loseWeight = function(a){
+		var ret = [];
+		for(var i,l=a.length;i<l;i++){
+			ret.push({
+				goodsId:a[i].goodsId,
+				count : a[i].count
+			});
+		}
+		return ret;
+	};
 	//接收打点
 	exports.add = function(req, res){
 		res.writeHead(200, {'Content-Type': 'text/plain'});
 		res.end();
 		beacon.add(req.query);
 	};
+	//轨迹页面
 	exports.init = function(req, res){
 		var pageId = req.params.pageId;
-		beacon.findByPageId(pageId,function(items){
-			res.render('beacon.ejs',{goodsList:items});
+		beacon.findByPageId(pageId,function(o){
+			res.render('track.ejs',{click:o.click,mouse:JSON.stringify(o.mouse.track),specialList:o.special.goods});
 		});
+	};
+	//结果页面
+	exports.result = function(req, res){
+		var pageId = req.params.pageId;
+		beacon.findResultByPageId(pageId,function(items){
+			res.render('beacon.ejs',{beaconList:items});
+		});
+	};
+	exports.follow = function(req, res){
+		var pageId = req.query.pageId;
+		beacon.findMouseByPageId(pageId,function(clickInfos,mouseInfo){
+			res.send({isRealTime:isRealTime,x:mouseInfo.x,y:mouseInfo.y,goodsInfo:loseWeight(clickInfos)});
+		});
+	};
+	exports.catch = function(req, res){
+		res.writeHead(200, {'Content-Type': 'text/plain'});
+		res.end();
+		beacon.catch(req.query);
 	};
 })();
